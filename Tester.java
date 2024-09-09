98eb289e654e6976c0fe0e2c3abd87876c5717d2
cinta <naurah_zanjabila_33rpl@student.smktelkom-mlg.sch.id>
2024-09-09 14:17:48 +0700

    Add files via upload

---
+++ Tester.java
@@ -0,0 +1,11 @@
+package Toko;
+
+public class Tester {
+    public static void main(String[] args) {
+        CD c1 = new CD();
+        c1.print();
+
+        DVD c2 = new DVD();
+        c2.print();
+    }
+}
